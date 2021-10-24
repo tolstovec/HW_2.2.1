528d1aa7aedf256d5455afcd3ea61c97f4281a6a
Ilya Pokhodin <ilya.pokhodin@gmail.com>
2021-10-24 14:09:22 +0300

    Программная часть полностью готова. Но побороть констрейнты до сих пор не удалось, поэтому на других айфонах всё сьезжает.

--- HW_2.2/ViewController.swift
+++ HW_2.2/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorWindowOutlet: UIView!
+    
+    @IBOutlet var redColorLabalOutlet: UILabel!
+    @IBOutlet var greenColorLabalOutlet: UILabel!
+    @IBOutlet var blueColorLabalOutlet: UILabel!
+    
+    @IBOutlet var redColorSliderOutlet: UISlider!
+    @IBOutlet var greenColorSliderOutlet: UISlider!
+    @IBOutlet var blueColorSliderOutlet: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorWindowOutlet.layer.cornerRadius = 20
     }
 
-
+    @IBAction func redColorSliderAction() {
+        redColorLabalOutlet.text = String(format: "%.2f", redColorSliderOutlet.value)
+        colorWindowOutlet.backgroundColor = UIColor(cgColor: CGColor.init(srgbRed:
+                                                                            CGFloat(redColorSliderOutlet.value), green: CGFloat(greenColorSliderOutlet.value), blue: CGFloat(blueColorSliderOutlet.value), alpha: 1))
+    }
+    
+    @IBAction func greenColorSliderAction() {
+        greenColorLabalOutlet.text = String(format: "%.2f", greenColorSliderOutlet.value)
+        colorWindowOutlet.backgroundColor = UIColor(cgColor: CGColor.init(srgbRed:
+                                                                            CGFloat(redColorSliderOutlet.value), green: CGFloat(greenColorSliderOutlet.value), blue: CGFloat(blueColorSliderOutlet.value), alpha: 1))
+    }
+    
+    @IBAction func blueColorSliderAction() {
+        blueColorLabalOutlet.text = String(format: "%.2f", blueColorSliderOutlet.value)
+        colorWindowOutlet.backgroundColor = UIColor(cgColor: CGColor.init(srgbRed:
+                                                                            CGFloat(redColorSliderOutlet.value), green: CGFloat(greenColorSliderOutlet.value), blue: CGFloat(blueColorSliderOutlet.value), alpha: 1))
+    }
+    
+    
 }
 
